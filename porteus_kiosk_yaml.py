5af8d12438bfba6e4fbd3ceeeae098f338147201
ehawman-rosenberg <ehawman@rosenberg-library.org>
2022-05-11 23:15:02 -0500

    refactor of warn_global and started export

--- porteus_kiosk_yaml.py
+++ porteus_kiosk_yaml.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 from ruamel.yaml import YAML, YAMLError
 from pathlib import Path
 from tabulate import tabulate
 import inflect
-from pprint import pprint
 
 
 def main():
@@ -15,7 +13,13 @@ def main():
 
     file = import_data(filename)
 
-    result = generate_result(file)
+    expanded = expand_groups(file)
+
+    warn_global(
+        file["CLIENTS"].values()
+    )  # Can any config parameters be moved to GLOBAL?
+
+    result = generate_result(expanded)
 
     export_data(filename, result)
 
@@ -31,74 +35,77 @@ def export_data(filename, result):
             print(exc)
 
 
-def generate_result(file):
+def generate_result(expanded):
+    """"""
+    result = []
+    result.append(generate_header("GLOBAL"))
+
+    # turn GLOBAL into a "client" and add it to (the front of) expanded["CLIENTS"]
+    # delete GLOBAL
+    # for each client (starting with GLOBAL) in expanded["CLIENTS"]
+    #   write the header
+    #   for key, value in sorted(client)
+    #       match type(value):
+    print("stop")
+
+
+def generate_header(client_name):
+    return f"[[ {client_name} ]]"
+
+
+def expand_groups(file):
     """Convert valid yml input into Porteus Kiosk ini configs"""
-    result = {}
-    # Loop through clients
-    for client_name, client_data in file["CLIENTS"].items():
-        client_data = populate_with_group(
-            client_data, file["GROUPS"]
-        )  # TODO find out how best to structure expanding/formatting/filtering this data (multiple functions)
-        result.update({client_name: client_data})
-    #     config_dict = result.values()
-    #     common_configs = set.intersection(*tuple(set(d.keys()) for d in config_dict))
-    #     identical_configs = [
-    #         {key: val}
-    #         for (key, val) in config_dict
-    #         if set(list(config_dict.values())) == val
-    #     ]
 
-    # print("yes")
-    warn_global(result)
+    if "GROUPS" in file:  # Do we even need to be here?
+        # Loop through clients
+        for client_name, client_data in file["CLIENTS"].items():
+            if "groups" in client_data:
+                for group in client_data["groups"]:
+                    if not group in file["GROUPS"]:
+                        raise KeyError(f"{group} is not a valid group!")
+                    for key, value in file["GROUPS"][group].items():
+                        client_data.update({key: value})
+                del client_data["groups"]
+            file["CLIENTS"].update({client_name: client_data})
+    del file["GROUPS"]
 
-    return result
+    return file
 
 
-def warn_global(result):
+def warn_global(values_dicts):
     """
     Alerts the user if one or more key:value pairs are shared amongst ALL clients,
     and are therefore candidates to be made GLOBAL configs.
     """
-    key_list = set(
-        [
-            key
-            for entry in [[*result[mac_addr].keys()] for mac_addr in result]
-            for key in entry
-        ]
-    )
-    by_key = {
-        key: [result[mac_addr].get(key) for mac_addr in result] for key in key_list
-    }
-    common_key_vals = {
-        key: [v for v in val if v is not None][0]
-        for key, val in by_key.items()
-        if all(item == [v for v in val if v is not None][0] for item in val)
-    }
+    # key_list = set(
+    #     [
+    #         key
+    #         for entry in [[*result[mac_addr].keys()] for mac_addr in result]
+    #         for key in entry
+    #     ]
+    # )
+    # by_key = {
+    #     key: [result[mac_addr].get(key) for mac_addr in result] for key in key_list
+    # }
+    # common_key_vals = {
+    #     key: [v for v in val if v is not None][0]
+    #     for key, val in by_key.items()
+    #     if all(item == [v for v in val if v is not None][0] for item in val)
+    # }
+    isect = dict(
+        pair
+        for pair in list(values_dicts)[0].items()
+        if all((pair in d.items() for d in list(values_dicts)[1:]))
+    )  # TODO troubleshoot
+
+    print(isect)
     p = inflect.engine()
     print(
-        f"The following {p.plural('setting',len(common_key_vals))} {p.plural('is',len(common_key_vals))} shared among all clients."
+        f'The following {p.plural("setting",len(common_key_vals))} {p.plural("is",len(common_key_vals))} shared among all clients.'
     )
     for key, value in common_key_vals.items():
         print(f"  - {key}: {value}")
     print("Consider making these configs GLOBAL.")
-
-
-def populate_with_group(client, groups):
-    """Adds group properties to a member client"""
-    for group in client["groups"]:
-        for key, value in groups[group].items():
-            if key not in client.keys():
-                client.update({key: value})
-            else:
-                print(
-                    f"{key} already exists in {client}! Which value would you like to keep?"
-                )
-                print(tabulate([[client[key], value]], headers=["Client", "Group"]))
-                answer = input("Which value should be kept? c/g ").lower()
-                if answer == "c":
-                    client.update({key: value})
-    del client["groups"]
-    return client
 
 
 def import_data(filename):
