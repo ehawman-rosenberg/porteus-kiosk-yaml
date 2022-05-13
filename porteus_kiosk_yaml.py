5daba820125349dbc224ed2f359a07c0b22a95da
Evan Hawman <ehawman@rosenberg-library.org>
2022-05-13 03:29:17 -0500

    working! needs schema

--- porteus_kiosk_yaml.py
+++ porteus_kiosk_yaml.py
@@ -5,19 +5,19 @@ from ruamel.yaml import YAML, YAMLError
 from pathlib import Path
 from tabulate import tabulate
 import inflect
-import json  # just for exporting sample data
+import clipops
 
 
 def main():
     """Main program"""
-    filename = "example_config.yml"
+    filename = "config.yml"  # TODO point this at some kind of file selector
 
-    file = import_data(filename)
+    file = import_data(filename)  # Loads up data
 
-    expanded = expand_groups(file)
+    expanded = expand_groups(file)  #
 
     warn_global(
-        list(file["CLIENTS"].values())
+        list(expanded.values())
     )  # Can any config parameters be moved to GLOBAL?
     result = generate_results(expanded)
 
@@ -27,62 +27,48 @@ def main():
 def export_data(filename, result):
     """Writes Porteus Kiosk ini content to file"""
     with open(Path(filename).stem + ".ini", "w+") as open_file:
-        try:
-            yaml = YAML(typ="safe")
-            yaml.default_flow_style = False
-            yaml.dump(result, open_file)
-        except YAMLError as exc:
-            print(exc)
+        open_file.write(result)
 
 
 def generate_results(expanded):
-    """"""
+    """Loops through clients, building the final ini string"""
     result = ""
-    result += client_to_ini({"GLOBAL": expanded["GLOBAL"]})
+    result += client_to_ini("GLOBAL", expanded["GLOBAL"])
     clients = expanded["CLIENTS"]
-    for client in dict(clients.items()):
-        client_to_ini(client)
-
-    # for each client (starting with GLOBAL) in expanded["CLIENTS"]
-    #   write the header
-    #   for key, value in sorted(client)
-    #       match type(value):
-    print("stop")
+    for name, configs in clients.items():
+        result += client_to_ini(name, configs)
     return result
 
 
-def client_to_ini(client):
-    """"""
+def client_to_ini(name, configs):
+    """Takes a client name and configs dict, returns a client formatted as Porteus Kiosk ini"""
     result = ""
-    ((client_name, client_configs),) = client.items()  # Split the top-level dict
-    result += f"[[ {client_name.upper()} ]]\n"
-    print(result)
-    for key, value in dict(sorted(client_configs.items())).items():
-        match type(value):  # This is what I need help with.
-            case "dict":
-                print("dict")
-                new_line = f"{key}="
-                for k, v in value:
-                    new_line += f"{v}|{k}| "
-                new_line = new_line[:-1]  # Trim the unneeded last space
-            case "list":
-                print("list")
-                new_line = f"{key}="
-                for i in value:
-                    new_line += f"{i}|"
-                new_line = new_line[:-1]  # Trim the unneeded last pipe
-            case _:
-                print(type(value))
-                new_line = f"{key}={value}"
-
-        print(f"{key}: {value}")
+    result += f"[[ {name.upper()} ]]\n"
+    for key, value in dict(sorted(configs.items())).items():
+        if type(value) == dict:
+            new_line = f"{key}="
+            for k, v in value.items():
+                new_line += f"{v}|{k}| "
+            new_line = new_line[:-1]  # Trim the unneeded last space
+        elif type(value) == list:
+            new_line = f"{key}="
+            if key == "homepage":
+                delim = "|"
+            else:
+                delim = " "
+            for i in value:
+                new_line += f"{i}{delim}"
+            new_line = new_line[:-1]  # Trim the unneeded last pipe
+        else:
+            new_line = f"{key}={value}"
         result += new_line + "\n"
+    result += "\n"
     print(result)
     return result
 
 
 def expand_groups(file):
-    """Convert valid yml input into Porteus Kiosk ini configs"""
+    """Loops through clients, adding the properties of their groups to their own"""
 
     if "GROUPS" in file:  # Do we even need to be here?
         # Loop through clients
@@ -109,6 +95,8 @@ def warn_global(values_dicts):
         for pair in values_dicts[0].items()
         if all((pair in d.items() for d in values_dicts[1:]))
     )
+    if not isect:
+        return
     p = inflect.engine()
     print(
         f'The following {p.plural("setting",len(isect))} {p.plural("is",len(isect))} shared among all clients.'
